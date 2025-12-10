432539d1ec55d91d091b7d958c47aed256c49db5
KennyNguyen04 <dungnguyen15112004@gmail.com>
2025-12-10 17:09:18 +0700

    Tạo nhân vật & Code nhảy 2 lần

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float jumpHeight = 7f;
+    public float moveSpeed = 5f;
+    private float movement;
+
+    public Transform groundCheckPoint;
+    public float groundCheckRadius = .2f;
+    public LayerMask whatIsGround;
+    private bool isGround;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isGround = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        Collider2D collInfo = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+        if (collInfo == true)
+        {
+            isGround = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(movement * moveSpeed, 0f, 0f) * Time.fixedDeltaTime;
+    }
+
+    void Jump()
+    {
+        if (isGround == true)
+        {
+            Vector2 velocity = rb.linearVelocity;
+            velocity.y = jumpHeight;
+            rb.linearVelocity = velocity;
+            isGround = false;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheckPoint == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheckPoint.position, groundCheckRadius);
+    }
+}
